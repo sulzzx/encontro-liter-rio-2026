3954f79360a4b772bd3a966fa79c3380fea803f8
sulzmgl <132315720+sulzzx@users.noreply.github.com>
2026-07-13 01:26:22 -0300

    Add files via upload

---
+++ vite.config.ts
@@ -0,0 +1,25 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+import { tanstackRouter } from "@tanstack/router-plugin/vite";
+
+export default defineConfig({
+  plugins: [
+    tanstackRouter({
+      target: "react",
+      autoCodeSplitting: true,
+    }),
+    tailwindcss(),
+    react(),
+  ],
+
+  server: {
+    port: 5173,
+  },
+
+  resolve: {
+    alias: {
+      "@": "/src",
+    },
+  },
+});
